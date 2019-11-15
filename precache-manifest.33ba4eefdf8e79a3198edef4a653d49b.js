cd3ef341e3f873f537a63880f9382d94ddc2fbd2
real-festil <53917776+real-festil@users.noreply.github.com>
2019-11-15 23:38:47 +0600

    Add files via upload

---
+++ precache-manifest.33ba4eefdf8e79a3198edef4a653d49b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ee348144b4b50e4ec8d70cf680fd21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd6a7f970d93003b90c",
+    "url": "/static/css/main.6513c17b.chunk.css"
+  },
+  {
+    "revision": "061a7780916fa32526f6",
+    "url": "/static/js/2.0c802ae1.chunk.js"
+  },
+  {
+    "revision": "bfd6a7f970d93003b90c",
+    "url": "/static/js/main.f260b573.chunk.js"
+  },
+  {
+    "revision": "b52a4cb25a8e1c0726c3",
+    "url": "/static/js/runtime-main.8ee05d18.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
